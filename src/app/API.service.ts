29e11884110ff948125b2857d795f5d87d70fd5b
Lara Newsom <lnewsom@sourceallies.com>
2021-11-11 20:39:16 -0600

    added aws amplify api

---
+++ src/app/API.service.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+//  This file was automatically generated and should not be edited.
+import { Injectable } from '@angular/core';
+import API, { graphqlOperation, GraphQLResult } from '@aws-amplify/api-graphql';
+import { Observable } from 'zen-observable-ts';
+
+export interface SubscriptionResponse<T> {
+  value: GraphQLResult<T>;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class APIService {}
--- src/app/models/cat.ts
+++ src/app/models/cat.ts
@@ -1,13 +1,15 @@
 export class Cat {
-    id: number = 0;
+    id: string = '';
     photoUrl: string = '';
     names: NameWithVote[] = [];
     votingEnds: number = (Date.now() + (1000 * 60 * 60 * 24 * 5));
 }
 
 export class NameWithVote {
+    id: string = '';
     name: string = '';
     votes: number = 0;
+    catId: string = '';
     constructor (name: string) {
         this.name =  name;
         this.votes = 0;
