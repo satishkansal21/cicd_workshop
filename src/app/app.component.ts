240b3e23df3db400ac56c05239ebfd0eab1fcfea
Lara Newsom <lnewsom@sourceallies.com>
2021-11-11 22:34:47 -0600

    removed auth from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { CognitoAuthUser, UserService } from './user.service';
-import { Auth } from 'aws-amplify';
-import { from, Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'name-that-cat-root',
@@ -10,12 +6,4 @@ import { tap } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  readonly auth = Auth;
-  readonly authUser$: Observable<CognitoAuthUser> = from(Auth.currentUserInfo()).pipe(
-    tap((user: CognitoAuthUser) => this.userService.setCurrentUser(user.username))
-  );
-  constructor(
-    private readonly userService: UserService
-  ) {
-  }
 }
